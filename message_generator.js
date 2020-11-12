ebdf095c7e340c16fcd6f065c4035bf76fc50550
ProgrammingBanana <daniel.jesus@upr.edu>
2020-11-12 15:48:48 -0400

    Add readline-sync for input and more jokes.

--- message_generator.js
+++ message_generator.js
@@ -1,28 +1,42 @@
+const readline = require('readline-sync');
+
 const jokeRandomizer = {
-    _whosThereAnswer: ['Yah',],
-    _punchLine: ['Naaah, bro, I prefer google.'],
+    _whosThereAnswer: ['Yah','Kenya','Dishes!'],
+    _punchLine: ['Naaah, bro, I prefer google.','Kenya feel the love tonight?', 'Dishes the Police come out with your hands up'],
 
     // Getter for 
     get whosThereAnswer(){
-        let answer = Math.floor(Math.random()*this._whosThereAnswer.length);
-        return this._whosThereAnswer[answer];    
+        return this._whosThereAnswer;    
     },
     get punchLine(){
-        let answer = Math.floor(Math.random()*this._punchLine.length);
-        return this._punchLine[answer];
+        return this._punchLine;
     },
     answerWho(answer){
         return `${answer} who?`;
     },
-    generateJoke(){
-        let answer = this.whosThereAnswer;
+    generateRandomJoke(){
+        let answer = this.whosThereAnswer[Math.floor(Math.random()*this._whosThereAnswer.length)];
+        let punchLineChoice = Math.floor(Math.random()*this._punchLine.length);
         console.log('Knock Knock!');
         console.log('Who\'s there?');
         console.log(answer);
         console.log(this.answerWho(answer));
         console.log('...')
-        console.log(this.punchLine);
+        console.log(this.punchLine[punchLineChoice]);
+    },
+    generateActualJoke(){
+        let index = Math.floor(Math.random()*this._whosThereAnswer.length);
+        let answer = this.whosThereAnswer[index];
+        let punchline = this.punchLine[index];
+        console.log('Knock Knock!');
+        console.log('Who\'s there?');
+        console.log(answer);
+        console.log(this.answerWho(answer));
+        console.log('...')
+        console.log(punchline);
     }
 }
 
-jokeRandomizer.generateJoke();
+let answer = readline.question('JOKE MENU:\n1. Nonsensical joke\n2. Actual Joke\nAnswer here:');
+if(answer === '1') jokeRandomizer.generateRandomJoke();
+else jokeRandomizer.generateActualJoke();
